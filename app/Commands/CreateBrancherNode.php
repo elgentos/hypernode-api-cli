b54e54f696d92230638cdee5bd39b4b3d61f0c82
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2023-03-27 16:32:39 +0200

    Added create brancher command

---
+++ app/Commands/CreateBrancherNode.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Commands;
+
+use Hypernode\Api\Exception\HypernodeApiClientException;
+use Hypernode\Api\Exception\HypernodeApiServerException;
+use Hypernode\Api\HypernodeClientFactory;
+use LaravelZero\Framework\Commands\Command;
+
+class CreateBrancherNode extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'brancher:create {hypernode} {--label= : Add labels to your Brancher node (comma-separated, optional)} {--clear-services= : Clear the data for provided service(s) when creating the Brancher instance. Useful when you don\'t want to run the production cron or supervisor. Can also be useful if you want to supply your own anonymized database. Possible options: cron, elasticsearch, mysql, supervisor (comma-separated, optional)}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Brancher node';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     * @throws HypernodeApiClientException
+     * @throws HypernodeApiServerException
+     */
+    public function handle()
+    {
+        $client = HypernodeClientFactory::create(config('hypernode.api_token'));
+
+        $originHypernode = $this->argument('hypernode');
+
+        $clearServices = array_filter(array_map('trim', explode(',', $this->option('clear-services'))));
+        $labels = array_filter(array_map('trim', explode(',', $this->option('label'))));
+
+        $client->brancherApp->create($originHypernode, [
+            'labels' => $labels,
+            'clear-services' => $clearServices,
+        ]);
+
+        $this->line('Brancher node creation for ' . $originHypernode . ' requested');
+    }
+}
