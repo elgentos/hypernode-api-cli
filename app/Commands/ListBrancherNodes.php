dd83704fdb50b9737961e998802316e98f88bd92
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2023-03-27 16:32:24 +0200

    Added table & json output format for list command

--- app/Commands/ListBrancherNodes.php
+++ app/Commands/ListBrancherNodes.php
@@ -2,9 +2,11 @@
 
 namespace App\Commands;
 
-use Illuminate\Console\Scheduling\Schedule;
+use Hypernode\Api\Exception\HypernodeApiClientException;
+use Hypernode\Api\Exception\HypernodeApiServerException;
 use LaravelZero\Framework\Commands\Command;
 use Hypernode\Api\HypernodeClientFactory;
+use Symfony\Component\Console\Helper\Table;
 
 class ListBrancherNodes extends Command
 {
@@ -13,7 +15,7 @@ class ListBrancherNodes extends Command
      *
      * @var string
      */
-    protected $signature = 'brancher:list {hypernode}';
+    protected $signature = 'brancher:list {hypernode} {--output= : Output format (json, default: table)}';
 
     /**
      * The description of the command.
@@ -26,6 +28,8 @@ class ListBrancherNodes extends Command
      * Execute the console command.
      *
      * @return mixed
+     * @throws HypernodeApiClientException
+     * @throws HypernodeApiServerException
      */
     public function handle()
     {
@@ -35,17 +39,32 @@ class ListBrancherNodes extends Command
 
         $client->brancherApp->list($originHypernode);
 
-        print_r($client->brancherApp->list($originHypernode));
+        $branchers = $client->brancherApp->list($originHypernode);
+
+        if ($this->option('output') === 'json') {
+            $this->line(json_encode($branchers, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+            return Command::SUCCESS;
+        }
+
+        $headers = array_keys($branchers[array_key_first($branchers)]);
+
+        $tableData = [];
+        foreach ($branchers as $brancher) {
+            $brancher['labels'] = array_keys($brancher['labels']);
+            foreach ($brancher as $key => $value) {
+                if (is_array($value)) $brancher[$key] = implode(', ', $value);
+            }
+            $tableData[] = $brancher;
+        }
+
+        $table = new Table($this->output);
+        $table
+            ->setHeaders(array_map('ucwords', $headers))
+            ->setRows($tableData);
+        $table->render();
+
+        return Command::SUCCESS;
     }
 
-    /**
-     * Define the command's schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
-    }
+
 }
